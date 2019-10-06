d68f910e3e4e2545fd600ba3faa50aebcf316094
jaskiran-98 <55691650+jaskiran-98@users.noreply.github.com>
2019-10-07 03:04:25 +0530

    Revert "set up"
    This reverts commit d56b1b1ce0215d87ba5b6d88339f3b48e6b5d637.

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,8 +1,4 @@
 module.exports.profile= function(req, res)
 {
-   // return res.end('<h1>Express is up for Codeial!</h1>');
-    return res.render('home', 
-    {
-        title: "Profile"
-    });
+    res.end('<h1>User Profile</h1>');
 };
