e587a6987c5a8dd6e77d9257c4ca0d1aebe86e13
jaskiran-98 <55691650+jaskiran-98@users.noreply.github.com>
2019-10-11 01:40:22 +0530

    new set up for sign-in and sign-up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,25 @@
 module.exports.profile= function(req, res)
 {
-    res.end('<h1>User Profile</h1>');
+    return res.render('user_profile', 
+    {
+        title: "Profile"
+    });
 };
+
+// render the sign up page
+module.exports.signUp= function(req, res)
+{
+    return res.render('user_sign_up',
+    {
+        title: "Codeial |Sign Up"
+    })
+}
+
+// render the sign in page
+module.exports.signIn= function(req, res)
+{
+    return res.render('user_sign_in',
+    {
+        title: "Codeial |Sign In"
+    })
+}
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express= require('express');
 const app= express();
 const port= 8000;
+const expressLayouts= require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
+app.use(expressLayouts);
+
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 
 //use express router
 app.use('/', require('./routes/index'));
@@ -8,6 +17,8 @@ app.use('/', require('./routes/index'));
 //set up the view engine
 app.set('view engine','ejs');
 app.set('views', './views');
+
+
 // app listens to the port
 app.listen(port, function(err)
 {
