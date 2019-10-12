879204821aa6aaa8a647175d5d1011b436178f07
jaskiran-98 <55691650+jaskiran-98@users.noreply.github.com>
2019-10-13 03:23:38 +0530

    set up for sign-up

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express= require('express');
+const cookieParser= require('cookie-parser');
 const app= express();
 const port= 8000;
 const expressLayouts= require('express-ejs-layouts');
+const db= require('./config/mongoose');
+
+app.use(express.urlencoded());
+app.use(cookieParser());
 
 app.use(express.static('./assets'));
 app.use(expressLayouts);
@@ -12,7 +17,7 @@ app.set('layout extractScripts', true);
 
 
 //use express router
-app.use('/', require('./routes/index'));
+app.use('/', require('./routes'));
 
 //set up the view engine
 app.set('view engine','ejs');
