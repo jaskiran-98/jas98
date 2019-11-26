4a4758f10ff3181a594d0b4faa27955f4fbc8dc6
jaskiran-98 <55691650+jaskiran-98@users.noreply.github.com>
2019-11-26 19:19:54 +0530

    added mailer

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,5 +1,6 @@
 const Comment= require('../models/comment');
 const Post= require('../models/post');
+const commentsMailer= require('../mailers/comments_mailer');
 
 module.exports.create= async function(req, res)
 {
@@ -11,16 +12,17 @@ module.exports.create= async function(req, res)
             let comment = await Comment.create({
                 content: req.body.content,
                 post: req.body.post,
-                user: req.user._id
+                user: req.user._id,
             });
 
             post.comments.push(comment);
             post.save();
 
+            comment = await comment.populate('user', 'name email').execPopulate();
+            commentsMailer.newComment(comment);
+
             if (req.xhr){
                 // Similar for comments to fetch the user's id!
-                comment = await comment.populate('user', 'name').execPopulate();
-    
                 return res.status(200).json({
                     data: {
                         comment: comment
---
+++ mailers/comments_mailer.js
@@ -0,0 +1,23 @@
+const nodeMailer= require('../config/nodemailer');
+
+// this is another way of exporting a method
+exports.newComment = (comment)=>{
+    // console.log('inside newComment mailer', comment);
+    let htmlString= nodeMailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs');
+
+    nodeMailer.transporter.sendMail({
+        from: 'jaskiransekhon.98@gmail.com',
+        to: comment.user.email,
+        subject: "New comment published",
+        html: htmlString
+    },(err, info)=>
+    {
+        if(err)
+        {
+            console.log('error in sending mail', err);
+            return;
+        }
+        console.log('Message sent', info);
+        return;
+    });
+}
